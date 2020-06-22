6f1e899bc9153de184705d8123baa70215302550
Keracy <fazzzle.de@gmail.com>
2020-06-22 10:59:45 +0300

    Uploading layout

---
+++ src/components/Profile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <main>
+            <img src="https://i.pinimg.com/originals/26/63/0e/26630ea960fd8485fd4a1d072356393a.jpg" />
+        </main>
+    )
+}
+
+export default Profile;
