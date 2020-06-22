5cb7c24cabbb9d5b2d6e8fbe3ba012970a5dd119
Keracy <fazzzle.de@gmail.com>
2020-06-22 15:50:12 +0300

    Style management

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
-
+import s from './Profile.module.css'
 const Profile = () => {
     return (
         <main>
             <img src="https://i.pinimg.com/originals/26/63/0e/26630ea960fd8485fd4a1d072356393a.jpg" />
+            <div>
+                ava+description
+            </div>
+            <div>
+                My posts
+            </div>
+            <div>
+                New Post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
         </main>
     )
 }
